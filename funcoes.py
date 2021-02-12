46923786b15028770da254107063c7d06f83784f
Cristian Zanella <74058519+CristianCompuTech@users.noreply.github.com>
2021-02-12 18:54:39 -0300

    PasswordGenerator-Python

--- funcoes.py
+++ funcoes.py
@@ -31,7 +31,7 @@ def gerador_senhas(quant, tam):
     cabecalho("GERANDO SENHAS...")
     sleep(1)
 
-    strings = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0","!","@","#","$","%","&","*")
+    strings = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0","!","@","#","$","%","&","*")
 
     for senha_gerada in range(0, int(quant)):
         for string in range(0, int(tam)):
